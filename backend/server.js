b8e00ca28fcb250a448bcf055a67d5dd9e8b9592
anusahu2509@gmail.com <anusahu2509@gmail.com>
2026-02-18 16:00:01 +0530

    fixed backend url for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config();
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
